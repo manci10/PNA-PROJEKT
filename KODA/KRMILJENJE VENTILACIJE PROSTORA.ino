6bcb6893ae9843b6d6a458a826b9ac29ed5fb651
manci10 <129843992+manci10@users.noreply.github.com>
2023-04-18 11:34:10 +0200

    Update KRMILJENJE VENTILACIJE PROSTORA.ino

--- KODA/KRMILJENJE VENTILACIJE PROSTORA.ino
+++ KODA/KRMILJENJE VENTILACIJE PROSTORA.ino
@@ -8,54 +8,56 @@
 
 void setup()
 {
-  pinMode(swVklop, INPUT);
-  pinMode(swMode, INPUT);
-  pinMode(pot, INPUT);
-  pinMode(temp, INPUT);
-  pinMode(nmos, OUTPUT);
-  pinMode(zelena, OUTPUT);
-  pinMode(rdeca, OUTPUT);
+  pinMode(swVklop, INPUT);
+  pinMode(swMode, INPUT);
+  pinMode(pot, INPUT);
+  pinMode(temp, INPUT);
+  pinMode(nmos, OUTPUT);
+  pinMode(zelena, OUTPUT);
+  pinMode(rdeca, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop()
 {
-  if(digitalRead(swVklop) == HIGH) //vklop
-  {
-    if(digitalRead(swMode) == HIGH) //ročno
-  {
-      digitalWrite(rdeca, HIGH);
-      digitalWrite(zelena, LOW);
-      int potVal = analogRead(pot);
-      int hitrost = 1023 - potVal;
-      digitalWrite(nmos, HIGH);
-      delayMicroseconds(hitrost);
-      digitalWrite(nmos, LOW);
-      delayMicroseconds(potVal);
-    }
-    else if (digitalRead(swMode) == LOW) //avtomatsko
-    {
-      digitalWrite(zelena, HIGH);
-      digitalWrite(rdeca, LOW);
-      int tempVal = analogRead(temp);
-      float tempHitrost = tempVal*5;
-      tempHitrost /= 1023;
-      float temperatura =(tempHitrost-0.5)*100;
-      if(temperatura <= 25)
-      {
-    analogWrite(nmos,50); //nizka hitrost
-   }
-      else if(temperatura>25 && temperatura<50)
-      {
-    analogWrite(nmos,100); //srednja hitrost
-      }
-      else
-      {
-    analogWrite(nmos,255); //polna hitrost
-   }
-    }
-  }
-  else
-  {
-  digitalWrite(nmos, LOW);
-  }
+  if(digitalRead(swVklop) == HIGH) //vklop
+  {
+    if(digitalRead(swMode) == HIGH) //ročno
+  {
+      digitalWrite(rdeca, HIGH);
+      digitalWrite(zelena, LOW);
+      int potVal = analogRead(pot);
+      int hitrost = 1023 - potVal;
+      digitalWrite(nmos, HIGH);
+      delayMicroseconds(hitrost);
+      digitalWrite(nmos, LOW);
+      delayMicroseconds(potVal);
+    }
+    else if (digitalRead(swMode) == LOW) //avtomatsko
+    {
+      digitalWrite(zelena, HIGH);
+      digitalWrite(rdeca, LOW);
+      int tempVal = analogRead(temp);
+      float tempHitrost = tempVal*5;
+      tempHitrost /= 1023;
+      float temperatura =(tempHitrost-0.5)*100;
+      if(temperatura <= 25)
+      {
+    analogWrite(nmos,50); //nizka hitrost
+   }
+      else if(temperatura>25 && temperatura<50)
+      {
+    analogWrite(nmos,100); //srednja hitrost
+      }
+      else
+      {
+    analogWrite(nmos,255); //polna hitrost
+   }
+    }
+  }
+  else
+  {
+  digitalWrite(nmos, LOW);
+  digitalWrite(zelena, LOW);
+  }
 }
